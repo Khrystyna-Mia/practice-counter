b9d0bc9a3f3ffcec7adff820b2acd3e24f96bda2
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-11 01:41:17 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,26 @@
-const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Container from './Container';
+import CounterView from '../views/CounterView';
+
+const App = () => (
+  <>
+    <ul>
+      <li>
+        <Link to="/counter">😎 Counter</Link>
+      </li>
+
+      <li>
+        <Link to="/todos">😎 Todos</Link>
+      </li>
+    </ul>
+
+    <Container>
+      <Routes>
+        <Route path="/counter" element={<CounterView />} />
+      </Routes>
+    </Container>
+  </>
+);
 
 export default App;
