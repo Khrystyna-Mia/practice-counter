b4cf2b064b24a4936260903bbc62653ceb8a83b5
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-11 23:29:18 +0300

    add

--- src/Redux/counter/store.jsx
+++ src/Redux/counter/store.jsx
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import counterReducer from './counter/counter-reducer';
+import counterReducer from './counter-reducer';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
